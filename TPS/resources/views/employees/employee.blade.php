d9d325c326992cfd5741f5f598e788f2d268d4d2
mkq48 <khuatquang1807@gmail.com>
2024-06-26 16:58:03 +0700

    Update .\employee.blade.php

--- TPS/resources/views/employees/employee.blade.php
+++ TPS/resources/views/employees/employee.blade.php
@@ -92,9 +92,9 @@
                     {{ $item->DOB}}
                 </td>
                 <td class="">
-                    <a href="{{ Route('employees.edit', $item)}}" class="text-decoration-none px-2 text-warning"><i class="bi bi-pencil"></i></a>
+                    <a href="{{ Route('employees.edit', $item)}}" class="text-decoration-none px-2 text-warning"><i class="bi bi-pencil-fill"></i></a>
                     <a href="#" class="text-decoration-none text-warning"  data-bs-toggle="modal" data-bs-target="#deleteModal{{ $item->Employ_ID}}">
-                        <i class="bi bi-trash3"></i>
+                        <i class="bi bi-trash3-fill"></i>
                     </a>
             
                 </td>
